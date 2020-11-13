22603c0605153629c5dfed00a44bae36092b7678
Alex <akeil@unc.edu>
2020-11-13 10:33:11 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#election2020
+![Lead over time](lead.png)
+
+
+A CSV file containing time-stamped voting updates in the 2020 US presidential election. Data originate from [Edison research](https://www.edisonresearch.com/election-polling/), but are compiled by the New York Times. 
+
+Each row is a date/time specific cumulative count of votes for each presidential candidate (aggregating 3rd party candidates). Note that cumulative counts occasionally go down, which has been misinterpreted as evidence of fraud by those who wish to find evidence of fraud. There is no indication in the data why these occur, but it is not unusual to see phenomena like that in data that are collected on-the-fly, regardless of the query. 
+
+Also included is the Julia file to extract the data and compile the CSV (please do not run indiscriminantly, as it runs a new query of the NYT website each time).
+
+Note that votes for each candidate are derived from an overall vote count and a fractional share, which leads to fractional votes, which have been rounded to 3 decimal places.
---
+++ extract_edison_data.jl
@@ -0,0 +1,92 @@
+# a program to extract Edison data from NYT website on 2020 US Presidential election
+using JSON, JSONTables, DataFrames, CSV, Dates, HTTP, Printf
+
+
+function readNYT(state="pennsylvania")
+  # pull data from NYT website in json format
+  dict2 = Dict()
+  st = "https://static01.nyt.com/elections-assets/2020/data/api/2020-11-03/race-page/$state/president.json"
+  HTTP.open(:GET, st) do f
+      dicttxt = read(f,String)  # file information to string
+      dict2 = JSON.parse(dicttxt)  # parse and transform data
+  end
+  dict2
+end
+
+function write_lines(res, fl; mode="a")
+  # arbitrary vector -> comma separate string -> line in a csv file
+  if typeof(res)==Array{Symbol,1}
+    outtext = join([String(a) for a in res], ",")
+  else
+    outtext = join(["$a" for a in res], ",")
+  end
+  open(fl, mode) do io
+     write(io, outtext*"\n")
+  end;
+end
+
+
+function tocsv(series, state;resfile="/tmp/votes.txt", mode="w")
+  # hacky function to extract tabular data from json data
+  if mode == "w" 
+    nms = ["state", "total_votes", "date", "time", "source", "biden_votes", "trump_votes", 
+           "thirdparty_votes", "biden_share", "trump_share", "thirdparty_share"]
+    write_lines(nms, resfile, mode="w")
+  end
+  for ff in series
+    votes = ff["votes"]
+    dtt = DateTime(ff["timestamp"][1:(end-1)])
+    date = Date(dtt)
+    time = Time(dtt)
+    source = ff["eevp_source"]
+    biden = 0.
+    trump = 0.
+    third = 0.
+    bidenshare = 0.
+    trumpshare = 0.
+    thirdshare = 0.
+    for candidate in ff["vote_shares"]
+      share = candidate[2]
+      if candidate[1] == "bidenj"
+        bidenshare += share
+        biden += round(votes*share, digits=3)
+      elseif candidate[1] == "trumpd"
+        trumpshare += share
+        trump += round(votes*share, digits=3)
+      end
+      thirdshare = round(1.0 - bidenshare - trumpshare, digits=3) 
+      third += round(votes*share, digits=3)    
+    end
+    res = hcat(state, votes, date, time, source, @sprintf("%.3f", biden), @sprintf("%.3f", trump), 
+               @sprintf("%.3f", third), bidenshare, trumpshare, thirdshare)
+    write_lines(res, resfile, mode="a")
+  end
+end
+
+function readallNYT(state="pennsylvania";resfile = "/tmp/voting.txt", mode="w")
+  # wrapper function to write csv for a state
+  fd = readNYT(state)
+  series = fd["data"]["races"][1]["timeseries"]
+  tocsv(series, state, resfile = resfile, mode=mode)
+end
+
+function main(outfile)
+  # wrapper function to write csv for all states + DC
+  nytstates = ["alabama", "alaska", "arizona", "arkansas", "california", "colorado", "connecticut", "delaware", 
+       "district-of-columbia", "florida", "georgia", "hawaii", "idaho", "illinois", "indiana", "iowa", 
+       "new-jersey", "kansas", "kentucky", "louisiana", "maine", "maryland", "massachusetts", "new-mexico", 
+       "michigan", "minnesota", "mississippi", "missouri", "montana", "nebraska", "nevada", "new-hampshire", 
+       "new-york", "north-carolina", "north-dakota", "ohio", "oklahoma", "oregon", "pennsylvania", "rhode-island", 
+       "south-carolina", "south-dakota", "tennessee", "texas", "utah", "vermont", "virginia", "washington", 
+       "west-virginia", "wisconsin", "wyoming"]
+  for (i,s) in enumerate(nytstates)
+    println("$s")
+    try
+      readallNYT(s, resfile = outfile, mode= i == 1 ? "w" : "a")
+    catch
+      println("Not found")
+    end
+  end
+end
+
+main(homedir()*"/repo/election2020/edison_json_data_nyt.csv")
